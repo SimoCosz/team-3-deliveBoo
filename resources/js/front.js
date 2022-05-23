4dc1058ed5ee90e16b6d5b596a01a83b82b71761
coszach@hotmail.it <coszach@hotmail.it>
2022-05-23 10:18:17 +0200

    init repo

---
+++ resources/js/front.js
@@ -0,0 +1,9 @@
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Vue = require('vue');
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+const app = new Vue({
+  el: '#app',
+});
