d7de6b10b7cc1154370025e790c3409f0ad56a11
coszach@hotmail.it <coszach@hotmail.it>
2022-05-26 16:55:41 +0200

    Compleate relation order and product

---
+++ database/migrations/2022_05_26_141218_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name', 100);
+            $table->string('client_surname', 100);
+            $table->string('client_address', 200);
+            $table->string('client_city', 80);
+            $table->string('client_phone', 10);
+            $table->string('client_email', 150);
+            $table->boolean('payment_state');
+            $table->double('total_price', 6,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
