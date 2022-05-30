8697767093e36b8e4937835a33361447ccfcae55
coszach@hotmail.it <coszach@hotmail.it>
2022-05-30 10:15:11 +0200

    fiexd create new product

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            CategorySeeder::class,
             UserSeeder::class,
             ProductSeeder::class,
             OrderSeeder::class,
-            CategorySeeder::class,
         ]);
     }
 }
