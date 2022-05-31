c5926f4e322922805cc2f371765c63d1b2068172
coszach@hotmail.it <coszach@hotmail.it>
2022-05-31 17:35:34 +0200

    init vue router

---
+++ resources/js/Pages/HomeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+<layout>
+  <div>
+    <img src="../../../public/img/logo-home.jpg" alt="">
+  </div>
+</layout>
+</template>
+
+<script>
+import Layout from '../layouts/Layout.vue'
+  export default {
+    components: { 
+      Layout
+    },
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,3 +2,12 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 Vue.use(VueRouter)
+import HomeComponent from '../Pages/HomeComponent.vue'
+
+const routes = [
+  {
+    path: 'home',
+    name: 'home',
+    component: HomeComponent
+  }
+]
