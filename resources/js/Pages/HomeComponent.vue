da859ca9bc595e54c8f2735cbad0483906c252c0
NicholasDevelop <marcaccini.n@gmail.com>
2022-06-01 18:03:44 +0200

    Caricate immagini + sezione jumbo conlcusa parzialmente

--- resources/js/Pages/HomeComponent.vue
+++ resources/js/Pages/HomeComponent.vue
@@ -1,18 +1,22 @@
 <template>
-<Layout>
-  <div>
-    <Users/>
-  </div>
-</Layout>
+  <Layout>
+    <div>
+      <Jumbo/>
+    </div>
+  </Layout>
 </template>
 
 <script>
   import Users from '../components/UsersIndex.vue'
   import Layout from '../layouts/Layout.vue'
+  import Jumbo from '../components/HomeJumbotron.vue'
+
+
   export default {
     components: { 
       Layout,
-      Users
+      Users,
+      Jumbo
     },
     
   }
---
+++ resources/js/components/HomeJumbotron.vue
@@ -0,0 +1,65 @@
+<template>
+    <section class="jumbo-container">
+        <div class="jumbo-sub-container">
+
+            <div class="jumbo-left">
+                <h1>I piatti che ami, a domicilio.</h1>
+            </div>
+
+            <div class="jumbo-right">
+                <img class="img-svg" src="../../../public/img/jumbo-bkg.svg" alt="">
+            </div>
+        </div>
+        
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.jumbo-container{
+    background-image: url('../../../public/img/background-jumbo.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 400px;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+
+    .jumbo-sub-container{
+        width: 1024px;
+        display: flex;
+        position: relative;
+
+        .jumbo-left{
+            width: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            h1{
+                color: #440063;
+                font-weight: 700;
+            }
+        }
+    
+        .jumbo-right{
+            width: 50%;
+            position: relative;
+            
+            .img-svg{
+                height: 350px;
+                position: absolute;
+                bottom: 0;
+                left: 50%;
+                transform: translateX(-50%);
+            }
+        }
+    }
+
+}
+</style>
