626d8fcc716fbfcf963b92444e90c38e58affa65
coszach@hotmail.it <coszach@hotmail.it>
2022-06-07 12:19:23 +0200

    compleate menu

--- resources/js/Pages/MenuPage.vue
+++ resources/js/Pages/MenuPage.vue
@@ -1,13 +1,40 @@
 <template>
-<div class="container">
-  <div v-for="product in user.products" :key="product.id">
-    {{product.name}}
+<Layout>
+  <div v-if="!loading">
+    <section class="info-users py-4">
+      <div class="my-container">
+        <div class="row">
+          <div class="col-12 col-md-4">
+            <figure>
+              <img class="restourant-image" :src=user.cover alt="">
+            </figure>
+          </div>
+          <div class="col-12 col-md-8">
+            <h1>{{user.name}}</h1>
+            <p>{{user.category}}</p>
+            <p>Roma</p>
+            <p>{{user.telephone_number}}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <ProductList :user="user"/>
+
   </div>
-</div>
+</Layout>
 </template>
 
 <script>
+import Layout from '../layouts/Layout.vue'
+import ProductList from '../components/ProductList.vue'
+
   export default {
+    components:{
+      Layout,
+      ProductList
+    },
+
     data(){
       return {
         user: null
@@ -21,10 +48,7 @@
             const { users } = res.data;
             this.user = users;
         })
-        .catch((err) => {
-          console.warn(err);
-            this.$router.push("/404");
-        });
+        
       }
     },
 
@@ -35,5 +59,16 @@
 </script>
 
 <style lang="scss" scoped>
-
+.info-users{
+  border-bottom: 1px solid rgba(0,0,0,.08);
+  box-shadow: 0 2px 4px rgb(0 0 0 / 10%);
+  .my-container{
+    max-width: 1500px;
+    margin: 0 auto;
+    padding: 0 20px;
+    .restourant-image{
+      border-radius: 4px;
+    }
+  }
+}
 </style>
