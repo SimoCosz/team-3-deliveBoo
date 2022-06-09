1eded0a2734f4345782444a58b6ce97633c9f309
coszach@hotmail.it <coszach@hotmail.it>
2022-06-09 13:55:24 +0200

    Implemented Stripe payment

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CheckoutController extends Controller
+{
+    public function checkout()
+    {   
+        // Enter Your Stripe Secret
+        \Stripe\Stripe::setApiKey('sk_test_51L8iyqJtHFYmGwezjEbVAp83vQMMMp7mRbrAZy7iOERejvIJ9HsHtFfRZr7zkhk5vJMoKnQCsL4FZLFURKTrOXjF00bYb4xsLs');
+        		
+		$amount = 100;
+		$amount *= 100;
+        $amount = (int) $amount;
+        
+        $payment_intent = \Stripe\PaymentIntent::create([
+			'description' => 'Stripe Test Payment',
+			'amount' => $amount,
+			'currency' => 'INR',
+			'description' => 'Payment From Codehunger',
+			'payment_method_types' => ['card'],
+		]);
+		$intent = $payment_intent->client_secret;
+
+		return view('credit-card',compact('intent'));
+
+    }
+
+    public function afterPayment()
+    {
+        echo 'Payment Has been Received';
+    }
+}
